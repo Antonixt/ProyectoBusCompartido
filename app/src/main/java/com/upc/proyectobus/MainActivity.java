29dae9c60f958c134deec161fdb2baddbeb9ae2b
Roel romero siguas <roelromerosiguas@gmail.com>
2023-02-06 19:49:22 -0500

    agregado x2

--- app/src/main/java/com/upc/proyectobus/MainActivity.java
+++ app/src/main/java/com/upc/proyectobus/MainActivity.java
@@ -1,15 +1,8 @@
 package com.upc.proyectobus;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,4 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
--- app/src/main/java/com/upc/proyectobus/asientos.java
+++ app/src/main/java/com/upc/proyectobus/asientos.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RegistroUsuario extends AppCompatActivity {
+public class asientos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registro_usuario);
+        setContentView(R.layout.activity_asientos);
     }
 }
