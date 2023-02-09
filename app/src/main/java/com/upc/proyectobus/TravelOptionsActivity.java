09fbae4cc68ffa35689e0bf5c44a570dbff6986a
Antonixt <101617041+Antonixt@users.noreply.github.com>
2023-02-08 20:50:35 -0500

    ingreso de modo

---
+++ app/src/main/java/com/upc/proyectobus/TravelOptionsActivity.java
@@ -0,0 +1,14 @@
+package com.upc.proyectobus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TravelOptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel_options);
+    }
+}
