1635493f021b88d33f9e8b07991afbcc3bde3b9c
Antonixt <101617041+Antonixt@users.noreply.github.com>
2023-02-10 22:48:22 -0500

    forma de pago

---
+++ app/src/main/java/com/upc/proyectobus/FormaPagoActivity.java
@@ -0,0 +1,14 @@
+package com.upc.proyectobus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormaPagoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_payment_method_open);
+    }
+}
